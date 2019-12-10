0d6a779bf9c7f6766ef990acfcde46593451dcd5
Sarah Amiri <sarah.amiri28@gmail.com>
2019-12-10 10:39:59 +0330

    private message enabling

--- chatRoom/clientChatroom.py
+++ chatRoom/clientChatroom.py
@@ -1,30 +1,43 @@
 import socket
-#import time
+import time
 import sys
 import threading
 
 IP = 'localhost'
-PORT = 8217
+PORT = 8201
 username = input("Enter your name: ")
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((IP, PORT))
-client_socket.setblocking(0)
+client_socket.send(bytes(username, 'utf-8'))
+isChosen = False
 
 
 def send_message():
     while True:
+        global isChosen
         msg = input()
         if msg:
-            client_socket.send(bytes(username + "->" + msg, 'utf-8'))
+            if not isChosen:
+                msg = '#' + username + '->' + msg
+                isChosen = True
+            elif msg == 'finish':
+                msg = '#finish'
+            else:
+                msg = username + "->" + msg
+            client_socket.send(bytes(msg, 'utf-8'))
+
+
+def first_setup():
+    mess = client_socket.recv(2048)
+    print(mess.decode('utf-8'))
+    client_socket.setblocking(0)
 
 
 t1 = threading.Thread(target=send_message)
 t1.start()
 
-# t2 = threading.Thread(target=hello)
-# t2.start()
 
-
+first_setup()
 while True:
     # #    message = client_socket.recv(1024)
     # #    print(message.decode('utf-8'))
@@ -38,7 +51,26 @@ while True:
             if not message:
                 print("Connection Closed!")
                 sys.exit()
-            print(message.decode('utf-8'))
+            message = message.decode('utf-8')
+            if message == '#OK':
+                print("Now you can start messaging")
+                print("If you want to close this private chat enter finish")
+                isChosen = True
+            elif message == '#busy':
+                print('Username you chose was busy')
+                isChosen = False
+            elif message == '#disconnect':
+                print('Username you chose was disconnected')
+                isChosen = False
+            elif message == '#exist':
+                print('Username does not exist')
+                isChosen = False
+            elif message[:7] == '#finish':
+                print('finish')
+                print(message[7:])
+                isChosen = False
+            else:
+                print(message)
 
     except IOError as e:
         pass
